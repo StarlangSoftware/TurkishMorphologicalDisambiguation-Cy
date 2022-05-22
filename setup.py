898e87fa446a5e06e8192694d1d2e88d84657aa2
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 22:44:14 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["MorphologicalDisambiguation/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-MorphologicalDisambiguation-Cy',
-    version='1.0.8',
+    version='1.0.9',
     packages=['MorphologicalDisambiguation', 'MorphologicalDisambiguation.data'],
     package_data={'MorphologicalDisambiguation': ['*.pxd', '*.pyx', '*.c'],
                   'MorphologicalDisambiguation.data': ['*.txt']},
